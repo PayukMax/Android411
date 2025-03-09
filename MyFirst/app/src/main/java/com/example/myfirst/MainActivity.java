21a4e0e7de07390ab1c8153aa60e85e79fca8d74
PayukMax <payukmax@gmail.com>
2025-03-09 18:20:36 +0700

    After lesson Android 3(1)...

--- MyFirst/app/src/main/java/com/example/myfirst/MainActivity.java
+++ MyFirst/app/src/main/java/com/example/myfirst/MainActivity.java
@@ -12,8 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textSample;
-private Button secButton;
+private TextView tS1;
+private Button but1;
+private boolean b1=false;
+private TextView tS2;
+private Button but2;
+private boolean b2=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,43 @@ private Button secButton;
 //        setContentView(R.layout.table_layout);
 //        setContentView(R.layout.frame_layout);
 //        setContentView(R.layout.button_panel);
-        setContentView(R.layout.thirth_layout);
-        textSample=findViewById(R.id.textSample);
-        secButton=findViewById(R.id.sButton);
-        secButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.hw3);
+
+        tS1=findViewById(R.id.textView28);
+        but1=findViewById(R.id.button15);
+        tS2=findViewById(R.id.textView29);
+        but2=findViewById(R.id.button16);
+
+        but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textSample.setText("Сам привет...");
-                textSample.setTextSize(50);
+                if (b1) {
+                    tS1.setText("Строка 1 надпись первая");
+                    but1.setText("Кнопка 1 надпись 2");
+                    b1=false;
+                } else {
+                    tS1.setText("Строка 1 надпись вторая");
+                    but1.setText("Кнопка 1 надпись 1");
+                    b1=true;
+                }
             }
         });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b2) {
+                    tS2.setText("Строка 2 надпись первая");
+                    but2.setText("Кнопка 2 надпись 2");
+                    b2=false;
+                } else {
+                    tS2.setText("Строка 2 надпись вторая");
+                    but2.setText("Кнопка 2 надпись 1");
+                    b2=true;
+                }
+            }
+        });
+
 
 //        setContentView(R.layout.sclorr_view);
 //        setContentView(R.layout.hw_layout);
@@ -46,8 +78,8 @@ private Button secButton;
 //        });
     }
 
-    public void sayHello(View view){
-        textSample.setText("Привет...");
-        textSample.setTextSize(25);
-    }
+//    public void sayHello(View view){
+//        textSample.setText("Привет...");
+//        textSample.setTextSize(25);
+//    }
 }
