ec5cac6c783d348da7a58205b1961da2a62cfedb
PayukMax <payukmax@gmail.com>
2025-06-05 19:55:30 +0700

    My 05062025...

--- FileApplication/app/src/main/java/com/example/fileapplication/MainActivity.java
+++ FileApplication/app/src/main/java/com/example/fileapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.fileapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,80 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String FILE_NAME = "content.txt";
+    Button btnSave, btnOpen;
+    EditText edText;
+    TextView text;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnSave = findViewById(R.id.save_text);
+        btnOpen = findViewById(R.id.open_text);
+        edText = findViewById(R.id.editor);
+        text = findViewById(R.id.text);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileOutputStream fos = null;
+                try {
+                    fos = openFileOutput(FILE_NAME, MODE_PRIVATE); //MODE_APPEND
+                    fos.write(edText.getText().toString().getBytes());
+                    Toast.makeText(MainActivity.this, "File saved...", Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                } finally {
+                    try {
+                        if (fos!=null)  fos.close();
+                    } catch (IOException e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+
+            }
         });
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileInputStream fis = null;
+                try {
+                    fis = openFileInput(FILE_NAME);
+                    byte[] bytes = new byte[fis.available()];
+                    fis.read(bytes);
+                    String txt = new String(bytes);
+                    text.setText(txt);
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                } finally {
+                    if (fis!=null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+
+            }
+        });
+
+
     }
 }
---
+++ FileApplication/app/src/main/java/com/example/fileapplication/MainActivity2.java
@@ -0,0 +1,82 @@
+package com.example.fileapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    private final static String FILE_NAME = "document.txt";
+    Button btnSave, btnOpen;
+    EditText edText;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        btnSave = findViewById(R.id.save_text);
+        btnOpen = findViewById(R.id.open_text);
+        edText = findViewById(R.id.editor);
+        text = findViewById(R.id.text);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try (FileOutputStream fos = new FileOutputStream(getExternalPath())) {
+                    String txt = edText.getText().toString();
+                    fos.write(txt.getBytes());
+                    Toast.makeText(MainActivity2.this, "File saved", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity2.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File file = getExternalPath();
+                if (!file.exists()) {
+                    Toast.makeText(MainActivity2.this, "No file", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    byte[] bytes = new byte[fis.available()];
+                    fis.read(bytes);
+                    text.setText(new String(bytes));
+
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity2.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    private File getExternalPath() {
+        return new File(getExternalFilesDir(null), FILE_NAME);
+    }
+}
